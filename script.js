f357aa8bd831a689f5b72d7fbd7602a0b710215f
DomenicoLerose85 <domenicolerose85@libero.it>
2022-03-30 18:21:38 +0200

    js_arrays_created

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const imageArray = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg',];
 
 const titles = ['Svezia', 'Svizzera', 'Inghilterra', 'Germania', 'Paradise'];
+
+let currentIndex = 0;
+
+const next = document.querySelector ('.next');
+
+console.log (imageArray, titles,);
